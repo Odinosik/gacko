8243642915cecd9a7bbb8e66f1cf655179716bcf
filip.glowacki@enigmasoi1.onmicrosoft.com <filip.glowacki@enigmasoi1.onmicrosoft.com>
2019-12-09 18:52:23 +0100

    rozroznianie kont wirtualnych

---
+++ GACKO.Shared/Models/VirtualAccount/VirtualAccountViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GACKO.Shared.Models.BankAccount;
+using GACKO.Shared.Models.Expense;
+using System.Collections.Generic;
+using GACKO.Shared.Models.Subscription;
+
+namespace GACKO.Shared.Models.VirtualAccount
+{
+    public class VirtualAccountViewModel
+    {
+        public VirtualAccountModel SelectedVirtualAccount { get; set; }
+        public IEnumerable<VirtualAccountModel> VirtualAccounts { get; set; }
+    }
+}
