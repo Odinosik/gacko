4b79aa9dc307acae9a4a7e641f28ae8b1112edde
filip.glowacki@enigmasoi1.onmicrosoft.com <filip.glowacki@enigmasoi1.onmicrosoft.com>
2019-12-09 20:27:12 +0100

    update

--- GACKO/wwwroot/js/bankacc.js
+++ GACKO/wwwroot/js/bankacc.js
@@ -1,11 +1,32 @@
-﻿$(".toggle").on("click", function () {
-    $(".newbankacc-container")
+﻿//$(".toggle").on("click", function () {
+//    $(".newbankacc-container")
+//        .stop()
+//        .addClass("active");
+//});
+
+//$(".close").on("click", function () {
+//    $(".newbankacc-container")
+//        .stop()
+//        .removeClass("active");
+//});
+
+
+$(".toggle").click(function(event) {
+    $(`#${event.target.id}.editbankacc-container`)
+        .stop()
+        .addClass("active");
+    $(`#${event.target.id}.newbankacc-container`)
         .stop()
         .addClass("active");
 });
 
-$(".close").on("click", function () {
-    $(".newbankacc-container")
+$(".close").click(function (event) {
+    $(`#${event.target.id}.editbankacc-container`)
+        .stop()
+        .removeClass("active");
+    $(`#${event.target.id}.newbankacc-container`)
         .stop()
         .removeClass("active");
 });
+
+$('input').inputfit();
